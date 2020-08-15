2ee3f66a4fb29213aa1f32a2e68f71bdabf54a1f
이유창 <yuchang@iyuchang-ui-MacBookPro.local>
2020-08-16 01:23:46 +0900

    Second commit

--- community/views.py
+++ community/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
+from .models import Question
+
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponse(request)
